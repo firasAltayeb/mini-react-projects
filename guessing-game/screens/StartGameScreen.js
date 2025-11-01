3ebadff7dcd17bcf4ff238a5e80b141b41a8098c
Firas Altayeb <tayebfiras@gmail.com>
2025-11-01 23:16:47 +0900

    Guessing Game: added ripple effect to primary button

--- guessing-game/screens/StartGameScreen.js
+++ guessing-game/screens/StartGameScreen.js
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: "#8a0c34ff",
+    backgroundColor: "#560720ff",
     borderRadius: 8,
     elevation: 4,
     shadowColor: "black",
