105e2124833d4776a6dc16321c9300ad214670b1
Firas Altayeb <tayebfiras@gmail.com>
2025-11-17 17:15:01 +0900

    Meals App: displayed a list of items

---
+++ meals-app/components/CategoryGridTile.js
@@ -0,0 +1,17 @@
+import { Pressable, View, StyleSheet, Text } from "react-native";
+
+function CategoryGridTile({ title, color }) {
+  return (
+    <View>
+      <Pressable>
+        <View>
+          <Text>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+export default CategoryGridTile;
+
+const styles = StyleSheet.create({});
