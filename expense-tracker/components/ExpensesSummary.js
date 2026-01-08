0531489d31a0acc7932fad8454af4048110a6460
Firas Altayeb <tayebfiras@gmail.com>
2026-01-08 18:45:18 +0900

    Expense Tracker: add ESLint and related devDependencies for code quality

--- expense-tracker/components/ExpensesSummary.js
+++ expense-tracker/components/ExpensesSummary.js
@@ -1,4 +1,4 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { GlobalStyles } from "../constants/styles";
 
 function ExpensesSummary({ expenses, periodName }) {
